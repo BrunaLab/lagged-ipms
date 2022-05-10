3bfede4ff5eee14867fa4bd2c35f7e52846e3d5c
Aariq <scottericr@gmail.com>
2022-05-10 13:45:12 -0400

    nevermind.  10 workers hits the QOS limit.  Guess we only have 9?

--- run.R
+++ run.R
@@ -1,2 +1,2 @@
 #to run as a background process in RStudio use the "Source" button and choose "Source as Local Job ..."
-targets::tar_make_clustermq(workers = 10L)
+targets::tar_make_clustermq(workers = 9L)
